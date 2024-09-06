688ab1872439b4a3c9cf578532cd5b03d961f47d
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-06 23:20:02 +0530

    useEffect to the rescue

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={10}
-        onSetRating={setMovieRating} // Make sure to pass setMovieRating
-      />
-      <p>This movie is rated {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}{" "}
-    <StarRating
-      maxRating={"ss"}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    {/*<App />*/}{" "}
-    <StarRating
-      maxRating={10}
-      color="red"
-      size={24}
-      className="test"
-      defaultRating={3}
-    />
-    <Test />
+    <App />
   </React.StrictMode>
 );
