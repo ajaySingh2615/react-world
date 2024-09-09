42546f2e1248947ed70f6af0851ada233d175212
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-10 00:22:38 +0530

    "Cleaning up the titles"

--- 06-usepopcorn/src/App.js
+++ 06-usepopcorn/src/App.js
@@ -299,6 +299,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId],
   );
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return function () {
+      document.title = "usePopcorn";
+    };
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
