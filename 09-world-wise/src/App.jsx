2db9aa4f8d18ada1dd9e4832d211d4d9be543fed
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-12 02:30:36 +0530

    Implemented main pages and routes

--- 09-world-wise/src/App.jsx
+++ 09-world-wise/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 function App() {
-    const x = 23;
-    return (
-        <div>This is first project using vite.</div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
